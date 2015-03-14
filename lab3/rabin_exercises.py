e89fe4fdbcede61c0118c8e61f54fb7350e1631b
Olexandr <kolexandr.13@gmail.com>
2015-03-14 21:55:36 +0200

    Lab 3: Exercise 4

--- lab3/rabin_exercises.py
+++ lab3/rabin_exercises.py
@@ -11,17 +11,18 @@ def main():
         'WALL': {"p": 53, "q": 67, "enc": '35382124'},
         'FREE': {"p": 53, "q": 71, "enc": '01161407'},
         'SOFT': {"p": 59, "q": 67, "enc": '17000557'},
-        'LOST': {"p": 71, "q": 31, "enc": '18330658'},
-        'SKIN': {"p": 71, "q": 67, "enc": '32844503'},
+        'TRUE': {"p": 71, "q": 31, "enc": '14201392'},
+        'DROP': {"p": 71, "q": 31, "enc": '14441516'},
         'STEP': {"p": 83, "q": 37, "enc": '12940249'},
         'IRON': {"p": 23, "q": 67, "enc": '02360974'},
         'COPY': {"p": 47, "q": 83, "enc": '28851481'},
-        'CASE': {"p": 83, "q": 23, "enc": '18201480'}
+        'EAST': {"p": 83, "q": 23, "enc": '15530464'}
     }
 
     for word in exercises.keys():
         obj = exercises[word]
         rabin = Rabin(obj["p"], obj["q"], alphabet)
+        # print(word + "(" + str(obj["enc"]) + ") = " + rabin.encode(word))
         print(word + "(" + str(obj["enc"]) + ") = " + str(rabin.decode(obj["enc"])))
 
 
