234c340c81482276fde7ed12eb62acba42752bce
Olexandr <kolexandr.13@gmail.com>
2015-03-14 19:36:44 +0200

    Lab 3: Exercise 4

--- lab3/ex4.py
+++ lab3/ex4.py
@@ -1,6 +1,6 @@
 from gmpy2 import powmod
-from lab3.ex3 import sqr, sqrt
-from lab3.inverse import inverse_x
+from lab3.ex3 import sqr
+from lab3.inverse import extended_euclidean_algorithm
 
 __author__ = 'Olexandr'
 
@@ -15,57 +15,76 @@ class Rabin:
         self.char_len = len(str(len(alphabet)))
 
     def decode(self, word):
-        words = []
         blocks = [int(word[i:i + self.block_len]) for i in range(0, len(word), self.block_len)]
-        for block in blocks:
-            q11, q12 = self.sqrt_v1(self.q, block)
-            q21, q22 = self.sqrt_v2(self.q, block)
-
-            p11, p12 = self.sqrt_v1(self.p, block)
-            p21, p22 = self.sqrt_v2(self.p, block)
-            q = 0
-            # x1, x2, x3, x4 = sqrt(self.p, self.q, int(block))
-            # words.append((block, self.decode_one(x1)))
-            # words.append((block, self.decode_one(x2)))
-            # words.append((block, self.decode_one(x3)))
-            # words.append((block, self.decode_one(x4)))
-        return str(words)
+        x = [set(map(lambda val: str(val).zfill(self.block_len), self.sqrt(self.p, self.q, block))) for block in blocks]
+        possible = []
+        sub_words = [self.decode_one(val) for val in x]
+        for w in sub_words:
+            if len(possible) == 0:
+                possible = w
+            else:
+                possible = ["".join(map(lambda v: v + wi, possible)) for wi in w]
+        return possible
 
     def decode_one(self, x):
-        chars = [int(str(x).zfill(self.block_len)[i:i + self.char_len]) for i in range(0, self.block_len, self.char_len)]
-        if len(list(filter(lambda code: code < len(self.alphabet), chars))) == 2:
-            return "".join(map(lambda code: self.alphabet[code], chars))
-        else:
-            return ""
+        sub_words = []
+        for val in x:
+            char_codes = [int(val[i:i + self.char_len]) for i in range(0, self.block_len, self.char_len)]
+            if len(list(filter(lambda code: code < len(self.alphabet), char_codes))) == 2:
+                sub_words.append("".join(map(lambda code: self.alphabet[code], char_codes)))
+        return sub_words
 
     def encode(self, word):
         code_str = "".join(map(lambda char: str(self.alphabet.index(char)).zfill(self.char_len), list(word)))
         blocks = [int(code_str[i:i + self.block_len]) for i in range(0, len(code_str), self.block_len)]
-        return "".join(map(lambda block: str(sqr(int(block), 2, self.n)).zfill(self.block_len), blocks))
+        return "".join(map(lambda block: str(sqr(self.p, self.q, int(block))).zfill(self.block_len), blocks))
+
+    @staticmethod
+    def sqrt_complex(p, x):
+        if (p - 3) % 4 == 0:
+            y = Rabin.sqrt_v1(p, x)
+        elif (p - 5) % 8 == 0:
+            y = Rabin.sqrt_v2(p, x)
+        else:
+            y = None
+        return y
+
+    @staticmethod
+    def sqrt(p, q, x):
+        x1, x2 = x % p, x % q
+        y1 = Rabin.sqrt_complex(p, x1)
+        y2 = Rabin.sqrt_complex(q, x2)
+
+        de, u, v = extended_euclidean_algorithm(p, q)
+        pairs = [(y1, y2), (-y1, y2), (y1, -y2), (-y1, -y2)]
+        return [(pair[1] * p * u + pair[0] * q * v) % (q * p) for pair in pairs]
 
     @staticmethod
     def sqrt_v1(p, x):
-        m = p - 3 * inverse_x(4, p)
+        m = int((p - 3) / 4)
         y = int(powmod(x, m + 1, p))
-        return y, p - y
+        return y % p
 
     @staticmethod
     def sqrt_v2(p, x):
-        m = p - 5 * inverse_x(8, p)
-        q = powmod(x, 2 * m + 1, p)
-        y = (int(powmod(x, m + 1, p)) * int(powmod(2, 2*m + 1, p))) % p
-        return y, p - y
+        m = int((p - 5) / 8)
+        if powmod(x, 2 * m + 1, p) == 1:
+            y = int(powmod(x, m + 1, p))
+        else:
+            y = int(powmod(x, m + 1, p) * powmod(2, 2 * m + 1, p))
+        return y % p
 
 
 def main():
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-    # encoded = "21672351"
-    encoded = "1497"
-    decoded = ""
-    # rabin = Rabin(67, 71, alphabet)
-    rabin = Rabin(53, 67, alphabet)
-    print(encoded + " = " + rabin.decode(encoded))
+    encoded = "21672351"
+    # encoded = "1497"
+    decoded = "NEXT"
+    rabin = Rabin(67, 71, alphabet)
+    # rabin = Rabin(53, 67, alphabet)
+
+    print(encoded + " = " + str(rabin.decode(encoded)))
     print(decoded + " = " + rabin.encode(decoded))
 
 
