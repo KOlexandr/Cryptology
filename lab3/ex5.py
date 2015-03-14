92357751bfb89c8af9775051bf46782c19dd78a3
Olexandr <kolexandr.13@gmail.com>
2015-03-14 20:55:58 +0200

    Lab 3: Exercise 5

--- lab3/ex5.py
+++ lab3/ex5.py
@@ -1,1 +1,17 @@
+from gmpy2 import powmod
+
 __author__ = 'Olexandr'
+
+
+def primitive_root(p):
+    return [n for n in range(1, p) if len(set([int(powmod(n, power, p)) for power in range(p - 1)])) == p - 1]
+
+
+def main():
+    numbers = [11, 13]
+    for val in numbers:
+        print(str(val) + " : " + ", ".join(map(str, primitive_root(val))))
+
+
+if __name__ == '__main__':
+    main()
